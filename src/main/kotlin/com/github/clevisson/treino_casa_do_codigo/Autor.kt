aa94e1a513fed40688ab897e65af0e0a02f20fcb
Bruno Ramos <brunobramos@gmail.com>
2025-12-07 16:21:33 -0300

    feat: aplicação de regex para tratar formato de email valido

--- src/main/kotlin/com/github/clevisson/treino_casa_do_codigo/Autor.kt
+++ src/main/kotlin/com/github/clevisson/treino_casa_do_codigo/Autor.kt
@@ -11,22 +11,24 @@ class Autor(
     val nome: String,
 
     @Column(nullable = false)
-    val descricao: String) {
+    val descricao: String
+) {
 
-        @Id
-        val emailFormatado = regexEmail(email.lowercase().trim())
+    @Id
+    val emailFormatado = email
+        .takeIf { regexEmail(email.lowercase().trim()) } ?: throw IllegalArgumentException("Email inválido: $email")
 
-        @Column(nullable = false)
-        val instante = Instant.now()
+    @Column(nullable = false)
+    val instante = Instant.now()
 
     override fun toString(): String {
         return ("Nome: $nome, Email: $emailFormatado, Descrição: $descricao, Instante da criação: $instante")
     }
 
-    companion object{
-        private fun regexEmail(email: String): Regex {
+    companion object {
+        private fun regexEmail(email: String): Boolean {
             val regex = "^[a-z0-9._+-]+[@][a-z0-9.-]+[.][a-z]{2,}([.][a-z0-9]+)?$"
-            return Regex(regex)
+            return Regex(regex).matches(email)
         }
     }
 }
